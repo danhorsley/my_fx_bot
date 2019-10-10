8f00f9038c578dd0775e5f4c6e43b2dfd7aad0fc
Daniel Horsley <dh@HallComm.local>
2019-10-10 16:05:45 +0100

    some db fucntionality

--- crawler/app.py
+++ crawler/app.py
@@ -1,12 +1,34 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-def create_app():
-    """Create and configure instance of the Flask application"""
-    app = Flask(__name__)
+APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+#APP.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+APP.config['ENV'] = 'debug'
+DB = SQLAlchemy(APP)
+
+@APP.route('/')
+def root():
+    """render base.html"""
+
+    return render_template('base.html')
+
+@APP.route('/refresh')
+def refresh():
+    """Pull fresh data from Open AQ and replace existing data."""
+    DB.drop_all()
+    DB.create_all()
+    return render_template('base.html')
+
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    price = DB.Column(DB.Float, nullable=False)
+    #lat = DB.Column(DB.Float)
+    #lon = DB.Column(DB.Float)
 
 
-    @app.route('/')
-    def barebones():
-        return render_template('base.html')
-
-    return app
+    def __repr__(self):
+        return '<Date :{}> <Price :{}>'.format(self.datetime,self.price)
