2d8d7c8d2dfce03ea8ad0ef987cbf70fb05c272b
danhorsley <34445739+danhorsley@users.noreply.github.com>
2019-11-05 17:34:54 +0000

    Update __init__.py

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -7,7 +7,7 @@ from decouple import config
 
 APP = Flask(__name__)
 
-APP.config['SECRET_KEY'] = config('DATABASE_URL')#os.environ.get('SECRET_KEY')
+APP.config['SECRET_KEY'] = config('SECRET_KEY')#os.environ.get('SECRET_KEY')
 #APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 APP.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')#os.environ.get('DATABASE_URL')
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
